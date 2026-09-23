c71171fdf48a0628e5440dfa1eaa30839537bbe1
Tuan Dat Quach <quachtuandat1903@gmail.com>
2026-09-23 23:06:18 +1000

    fixed task 1 for week 7 lab

--- myfirst.php
+++ myfirst.php
@@ -7,17 +7,17 @@
 </head>
 
 <body>
-    <h1> PHP Variables, arrays and operators </hl>
+    <h1> PHP Variables, arrays and operators </h1>
     <?php
     $marks = array (85, 85, 95); // declare and initialise array
     $marks [1] = 90;
-    /l modify second element
+    // modify second element
     $ave = ($marks[0] + $marks[1] + $marks[2])/3; //Compute Average
-    if (Save >= 50)
+    if ($ave >= 50)
     $status = "PASSED";
     else 
     $status = "FAILED";
-    echo "<p> The average score is $ave. You $status.< /p>";
+    echo "<p> The average score is $ave. You $status.</p>";
     ?>
 </body>
 </html>
